0a21e83b7d7d86cee23ca4570eaff17cdd05dc90
Kethlyn <kethlynsaraa@gmail.com>
2022-07-25 15:31:22 -0300

    prepare for deploy

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from "./app/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = +process.env.PORT || 5000;
+const port = +process.env.PORT || 7000;
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
